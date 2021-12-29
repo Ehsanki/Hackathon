800dab0823c1c2906e1f868aa11e6176f61a63ed
Admin <ehsankittany@gmail.com>
2021-12-29 12:45:22 +0200

    almost there

--- Client.py
+++ Client.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import struct
-import  msvcrt
+
 
 
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- Server.py
+++ Server.py
@@ -22,16 +22,16 @@ def run_Server(server_port, broadcast_port):
     ServerSocket = socket.socket()  # get instance a
     ServerSocket.bind(("", server_port))  # bind host address and port together
     ServerSocket.setblocking(False)  # set socket to non-blocking mode
-    ServerSocket.listen(0)  # configure how many client the server can listen simultaneously
+    ServerSocket.listen(1)  # configure how many client the server can listen simultaneously
 
-   
+
     while True: 
         stop_broadcast = time.time() + 10
         while time.time() < stop_broadcast:
             BroadCastSocket.sendto(message, ('<broadcast>', broadcast_port)) # invitaions send
             
             try:
-              
+            
                 conn, address = ServerSocket.accept() # accept new connection
                 print("Connection from: " + str(address) )
     
@@ -111,14 +111,20 @@ def startGame():
       
         try:
             data = player1.recv(1024).decode('utf-8')  # receive response #leeeh?
-            print("wow player 1  pressed !!! "+ data)
+            if data=="4":
+                player1.send(b"CONGRATS CHAPMION YOU HAVE WON!!!!!!!!!")
+                player2.send(b"BETTER LUCK NEXT TIME TIME YOU HAVE LOST LOSER ! ")
             ## IF WIN SEND SOMETHING
 
             DrawFlag=False
+            break
 
         except ConnectionResetError:
             print("Player 1  Disconnected Game over ")
-            break
+            player2.close()
+            numClients=0
+            clients=[]
+            return
         except: ## DIDNT ANSWER YET 
             pass
 
@@ -126,18 +132,28 @@ def startGame():
             data = player2.recv(1024).decode('utf-8')  # receive response
             print("wow player 2  pressed !!! "+ data)
             ## IF WIN SEND SOMETHING
-
+            if data=="4":
+                player2.send(b"CONGRATS CHAPMION YOU HAVE WON!!!!!!!!!")
+                player1.send(b"BETTER LUCK NEXT TIME TIME YOU HAVE LOST LOSER ! ")
             DrawFlag=False
+            break
 
 
         except ConnectionResetError:
             player1.close()
-            print("Player 2 Disconnected Game over ")
-            break
+            print("Player 2  Disconnected Game over ")
+            player2.close()
+            numClients=0
+            clients=[]
+            return
+
+
+            
         except:## DIDNT ANSWER YET 
             pass
     
     # NEED TO CHECK
+    """
     th1=threading.Thread(target=empty_socket,args=(player1,))
     th2=threading.Thread(target=empty_socket,args=(player2,)) 
 
@@ -146,6 +162,7 @@ def startGame():
 
     th1.join()
     th2.join()
+    """
 
 
 
